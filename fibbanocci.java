77e96cd6cef5d2b64c1a9aa901040f1cf1cf0151
BindhushaBheemana <125570277+BindhushaBheemana@users.noreply.github.com>
2025-03-08 14:48:37 +0530

    fibbanocci.java

--- fibbanocci.java
+++ fibbanocci.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
 public class fibbanocci {
-    public static void printFibonacci(int n) {
-        int a = 0, b = 1;
-        for (int i = 0; i < n; i++) {
-            System.out.print(a + " ");
-            int c = a + b;
-            a = b;
-            b = c;
+    
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n; 
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2); 
         }
     }
-
+    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        printFibonacci(n);
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       for (int i = 0; i < n; i++) {
+        System.out.print(fibonacci(i) + " ");
+    }
     }
 }
